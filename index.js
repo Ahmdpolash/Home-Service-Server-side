4573d2d86806209fb5d97b4e15cc1bdff06ac65f
Polash <ahmedpolash732@gmail.com>
2023-11-07 14:11:57 -0600

    change some mistake

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("houseService").collection("services");
+    const bookingCollection = client.db("houseService").collection("bookings");
 
     //!get all service
     app.get("/api/services", async (req, res) => {
@@ -58,41 +59,70 @@ async function run() {
     });
 
     //!post to collection (add service)
+
     app.post("/api/services", async (req, res) => {
       const services = req.body;
       const result = await serviceCollection.insertOne(services);
       res.send(result);
     });
 
+    //!bookings (email)
 
+    app.post("/api/bookings", async (req, res) => {
+      const bookings = req.body;
+      console.log(bookings);
+      const result = await bookingCollection.insertOne(bookings);
+      res.send(result);
+    });
 
-    //!delete 
-    app.delete('/api/services/delete/:id',async(req,res) =>{
-      const id = req.params.id 
-      const query = {_id : new ObjectId(id)}
-      const result = await serviceCollection.deleteOne(query)
-      res.send(result)
-    })
+    app.get("/api/bookings", async (req, res) => {
+      const result = await bookingCollection.find().toArray();
+      res.send(result);
+    });
+
+    //!{email}
+
+    app.get("/api/bookings/:userEmail", async (req, res) => {
+      console.log(req.query.email);
+    
+      const userEmail = req.params.userEmail; 
+
+      const result = await bookingCollection.find({userEmail}).toArray();
+      res.send(result);
+
+    });
+
+    //!delete
+    app.delete("/api/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+
+  
 
     //!update
-    app.put('/api/services/update/:id', async(req,res) =>{
-      const id = req.params.id
-      const data = req.body ;
-      const filter = {id: new ObjectId(id)}
-      const options = {upsert : true}
+    app.put("/api/services/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { id: new ObjectId(id) };
+      const options = { upsert: true };
       const updateService = {
-        $set:{
-          name : data.service_name ,
-          image : data.service_image,
-          description : data.description ,
-          price : data.price 
-        }
-      }
-      const result = await serviceCollection.updateOne(filter,updateService,options)
-      res.send(result)
-    })
-
-
+        $set: {
+          name: data.service_name,
+          image: data.service_image,
+          description: data.description,
+          price: data.price,
+        },
+      };
+      const result = await serviceCollection.updateOne(
+        filter,
+        updateService,
+        options
+      );
+      res.send(result);
+    });
 
     //!jwt
     app.post("/jwt", async (req, res) => {
